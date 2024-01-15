bc7f6bbae07681fa2e7c4ae0ae416d6b151a3d72
IshfaqAhmedProg <manslayeriam@gmail.com>
2024-01-15 22:23:07 +0400

    fixed fille name issue

---
+++ contexts/ResponsiveContext.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { useMediaQuery, useTheme } from "@mui/material";
+import { createContext, useContext } from "react";
+
+const ResponsiveContext = createContext<any>({});
+export const useResponsive = (): IResponsiveContext =>
+  useContext(ResponsiveContext);
+export default function ResponsiveContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const theme = useTheme();
+  const matchesMD = useMediaQuery(theme.breakpoints.up("md"));
+  const matchesLG = useMediaQuery(theme.breakpoints.up("lg"));
+  const matchesXL = useMediaQuery(theme.breakpoints.up("xl"));
+  const matchesXXL = useMediaQuery(theme.breakpoints.up("xxl"));
+  return (
+    <ResponsiveContext.Provider
+      value={{ matchesMD, matchesLG, matchesXL, matchesXXL }}
+    >
+      {children}
+    </ResponsiveContext.Provider>
+  );
+}
+interface IResponsiveContext {
+  matchesMD: boolean;
+  matchesLG: boolean;
+  matchesXL: boolean;
+  matchesXXL: boolean;
+}
